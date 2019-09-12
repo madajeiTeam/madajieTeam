82079db630a292bdf9955653d2254afcd9564bc3
王炳瑞 <15174293@qq.com>
2019-09-12 15:08:19 +0800

    wbr

--- src/router.js
+++ src/router.js
@@ -1,12 +1,16 @@
 import React,{Component} from 'react'
 import {HashRouter,Route,Switch,Redirect} from 'react-router-dom'
 import  App from  './App'
+import Wbr from 'pages/wbr'
 class RootRouter extends Component{
     render() {
         return(
             <App>
                 <HashRouter>
-
+                    <Switch>
+                    {/* <Redirect exact from = '/' to ='/wbr'> </Redirect> */}
+                        <Route path="/wbr" component={Wbr}></Route>
+                    </Switch>
                 </HashRouter>
             </App>
         )
