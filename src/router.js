af1ce9b6544ad31e97f12cc6a18914909da7f6f2
王洋 <wy6666888@126.com>
2019-09-12 15:08:17 +0800

    wy

--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
 import React,{Component} from 'react'
 import {HashRouter,Route,Switch,Redirect} from 'react-router-dom'
 import  App from  './App'
+import Home from './wy/index'
 class RootRouter extends Component{
     render() {
         return(
             <App>
                 <HashRouter>
-
+                    <Switch>
+                        <Route path='/home' component={Home}>Hello</Route>
+                    </Switch>
                 </HashRouter>
             </App>
         )
